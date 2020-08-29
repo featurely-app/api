31263603e13f24617ff45ec17110101b7c0abe3a
thetutlage <virk.officials@gmail.com>
2020-08-29 10:31:50 +0530

    refactor: manually handle E_ROW_NOT_FOUND exception

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -14,10 +14,21 @@
 */
 
 import Logger from '@ioc:Adonis/Core/Logger'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
 	constructor() {
 		super(Logger)
+	}
+
+	public async handle(error: any, ctx: HttpContextContract) {
+		if (error.code === 'E_ROW_NOT_FOUND') {
+			ctx.response.send({
+				errors: [{
+					message: 'Resource not found'
+				}]
+			})
+		}
 	}
 }
