7f4f46e99d09a535a9b0190b18eac6f8fb9ab351
thetutlage <virk.officials@gmail.com>
2020-08-31 20:10:30 +0530

    format source files

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -51,9 +51,12 @@ export default class UsersController {
 			.post()
 
 		if (!accessToken.body.access_token) {
-			response.redirect().withQs({
-				error: `Request to authenticate with ${params.provider} has failed`,
-			}).toPath(redirectUrl)
+			response
+				.redirect()
+				.withQs({
+					error: `Request to authenticate with ${params.provider} has failed`,
+				})
+				.toPath(redirectUrl)
 			return
 		}
 
@@ -75,14 +78,13 @@ export default class UsersController {
 
 		if (!email) {
 			const emails = await new Oauth2()
-			.getUserRequest('https://api.github.com/user/emails')
-			.field('client_id', Config.get('services.github.clientId'))
-			.field('client_secret', Config.get('services.github.clientSecret'))
-			.acceptJSON()
-			.sendJSON()
-			.header('Authorization', `token ${accessToken.body.access_token}`)
-			.get()
-
+				.getUserRequest('https://api.github.com/user/emails')
+				.field('client_id', Config.get('services.github.clientId'))
+				.field('client_secret', Config.get('services.github.clientSecret'))
+				.acceptJSON()
+				.sendJSON()
+				.header('Authorization', `token ${accessToken.body.access_token}`)
+				.get()
 
 			if (Array.isArray(emails.body)) {
 				const verifiedEmail = emails.body.find((row: any) => row && row.verified === true)
@@ -94,9 +96,12 @@ export default class UsersController {
 		 * Still unable to find email address
 		 */
 		if (!email) {
-			response.redirect().withQs({
-				error: `Your privacy settings doesnt allow ${params.provider} to share your email with us`,
-			}).toPath(redirectUrl)
+			response
+				.redirect()
+				.withQs({
+					error: `Your privacy settings doesnt allow ${params.provider} to share your email with us`,
+				})
+				.toPath(redirectUrl)
 			return
 		}
 
@@ -112,19 +117,22 @@ export default class UsersController {
 			}
 		)
 
-		response.redirect().withQs({
-			exchange_url: Route.makeSignedUrl('UsersController.exchangeToken', {
-				qs: {
-					id: user.id
-				},
+		response
+			.redirect()
+			.withQs({
+				exchange_url: Route.makeSignedUrl('UsersController.exchangeToken', {
+					qs: {
+						id: user.id,
+					},
+				}),
 			})
-		}).toPath(redirectUrl)
+			.toPath(redirectUrl)
 	}
 
 	public async exchangeToken({ request, response, auth }: HttpContextContract) {
 		if (!request.hasValidSignature()) {
 			return response.status(400).send({
-				errors: [{ message: 'Invalid code' }]
+				errors: [{ message: 'Invalid code' }],
 			})
 		}
 
--- database/seeders/App.ts
+++ database/seeders/App.ts
@@ -1,4 +1,4 @@
-import { join }  from 'path'
+import { join } from 'path'
 import { readFile } from 'fs-extra'
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import { TenantFactory } from 'Database/factories/User'
@@ -12,24 +12,26 @@ export default class AppSeeder extends BaseSeeder {
 			const tenant = await TenantFactory.merge({
 				subdomain: 'adonisjs',
 				businessName: 'AdonisJS',
-			}).client(trx).with('owner', 1, (factory) => {
-				factory.merge({
-					fullName: 'Harminder Virk',
-					email: 'virk@adonisjs.com',
-					password: 'secret',
-					accountSource: 'password',
+			})
+				.client(trx)
+				.with('owner', 1, (factory) => {
+					factory.merge({
+						fullName: 'Harminder Virk',
+						email: 'virk@adonisjs.com',
+						password: 'secret',
+						accountSource: 'password',
+					})
 				})
-			}).create()
+				.create()
 
 			tenant.useTransaction(trx)
 
-			const project = await tenant
-				.related('projects')
-				.create({
-					name: 'AdonisJS v5',
-					description: 'Project covers the tasks to be accomplished in order to release the final stable version of AdonisJS v5',
-					logoUrl: 'https://preview.adonisjs.com/img/adonis-banner.svg',
-				})
+			const project = await tenant.related('projects').create({
+				name: 'AdonisJS v5',
+				description:
+					'Project covers the tasks to be accomplished in order to release the final stable version of AdonisJS v5',
+				logoUrl: 'https://preview.adonisjs.com/img/adonis-banner.svg',
+			})
 
 			const statuses = await project.related('statuses').createMany([
 				{
@@ -58,23 +60,25 @@ export default class AppSeeder extends BaseSeeder {
 				},
 			])
 
-			await project
-				.related('posts')
-				.create({
-					userId: tenant.owner.id,
-					phaseId: statuses[2].id,
-					title: 'Adding support for validations and cache in Env provider',
-					description: await readFile(join(__dirname, '..', 'fixtures', 'env-validations.md'), 'utf-8'),
-				})
+			await project.related('posts').create({
+				userId: tenant.owner.id,
+				phaseId: statuses[2].id,
+				title: 'Adding support for validations and cache in Env provider',
+				description: await readFile(
+					join(__dirname, '..', 'fixtures', 'env-validations.md'),
+					'utf-8'
+				),
+			})
 
-			await project
-				.related('posts')
-				.create({
-					userId: tenant.owner.id,
-					phaseId: statuses[3].id,
-					title: 'Collect.js - Add Laravel Collections (Illuminate/Collections) to Adonis',
-					description: await readFile(join(__dirname, '..', 'fixtures', 'laravel-collections.md'), 'utf-8'),
-				})
+			await project.related('posts').create({
+				userId: tenant.owner.id,
+				phaseId: statuses[3].id,
+				title: 'Collect.js - Add Laravel Collections (Illuminate/Collections) to Adonis',
+				description: await readFile(
+					join(__dirname, '..', 'fixtures', 'laravel-collections.md'),
+					'utf-8'
+				),
+			})
 		})
 	}
 }
